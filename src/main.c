3dc720ddb53acf65f6216c9bd89a4d6ea282d348
Nick Sanford <nicholascsanford@gmail.com>
2024-04-14 18:50:42 -0400

    Implement moving shapes

--- src/main.c
+++ src/main.c
@@ -4,14 +4,35 @@ int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "raylib hello world");
+  InitWindow(screenWidth, screenHeight, "raylib hello world - keyboard input");
+
+  Vector2 ballPosition = {(float)screenWidth / 2, (float)screenHeight / 2};
 
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_RIGHT))
+      ballPosition.x += 2.0f;
+    if (IsKeyDown(KEY_LEFT))
+      ballPosition.x -= 2.0f;
+    if (IsKeyDown(KEY_DOWN))
+      ballPosition.y += 2.0f;
+    if (IsKeyDown(KEY_UP))
+      ballPosition.y -= 2.0f;
+
+    if (IsKeyDown(KEY_L))
+      ballPosition.x += 2.0f;
+    if (IsKeyDown(KEY_H))
+      ballPosition.x -= 2.0f;
+    if (IsKeyDown(KEY_J))
+      ballPosition.y += 2.0f;
+    if (IsKeyDown(KEY_K))
+      ballPosition.y -= 2.0f;
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Hello World!", 190, 200, 20, LIGHTGRAY);
+    DrawText("move the ball with the arrow keys or HJKL", 10, 10, 20, DARKGRAY);
+    DrawCircleV(ballPosition, 50, MAROON);
     EndDrawing();
   }
 
